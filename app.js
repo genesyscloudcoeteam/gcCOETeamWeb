7d52f7c419b2b78757dcb72535b26fdb41d22cc5
poithead <poithead@users.noreply.github.com>
2024-11-29 17:34:30 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
