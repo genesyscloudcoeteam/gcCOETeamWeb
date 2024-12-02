206abeeda94c57d9f28ee0fcea91b86a8a5b50f0
poithead <poithead@users.noreply.github.com>
2024-12-02 10:41:50 +0200

    Look and feel update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
+      <h1>Demo Retail Website</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/shop">Shop</Link></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/login">Login</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
       </ul>
     </nav>
   );
