96bbbeebf25a4c9cba013e720a818ff2f8e3e620
poithead <poithead@users.noreply.github.com>
2024-12-05 11:45:56 +0200

    Register and Login Updates

--- src/components/RegisterModal.js
+++ src/components/RegisterModal.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const RegisterModal = ({ onClose }) => {
+const RegisterModal = ({ cookieConsent, onClose }) => {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
