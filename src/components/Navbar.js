a49e5166b181e07d20d1cd5ca567f0a05bc58a77
poithead <poithead@users.noreply.github.com>
2024-12-06 12:15:22 +0200

    Register and Login Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 /* global Genesys */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useNavigate } from "react";
 import { Link } from "react-router-dom";
 import { executeGenesysCommand } from "../utils/genesysHelper";
 
