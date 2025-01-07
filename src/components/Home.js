72b85aca3a2f75942ce77561bb645485624f1d9e
poithead <poithead@users.noreply.github.com>
2025-01-07 15:15:36 +0200

    home page image

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,17 @@ import { executeGenesysCommand } from "../utils/genesysHelper";
 const Home = () => {
   return (
     <div className="home-page">
-      <h1>Welcome to Demo Retail Store</h1>
-      <p style="text-align: center">Your one-stop shop for all things amazing!</p>
-      <img
-      src={`${process.env.PUBLIC_URL}/images/retailImages.jpg`} // Use PUBLIC_URL
-      alt="Homepage"
-    />
+      <div className="home-content">
+        <h1>Welcome to Demo Retail Store</h1>
+        <p>Your one-stop shop for all things amazing!</p>
+        <div className="image-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/retailImages.jpg`} // Use PUBLIC_URL
+            alt="Homepage"
+            className="fade-image"
+          />
+        </div>
+      </div>
     </div>
   );
 };
