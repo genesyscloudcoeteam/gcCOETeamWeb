4842d508c3b85d2c7448677b092259c3b7daecb6
poithead <poithead@users.noreply.github.com>
2025-01-07 14:21:21 +0200

    home page image

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <div className="home-page">
       <h1>Welcome to Demo Retail Store</h1>
-      <p className="homepage-paragraph">Your one-stop shop for all things amazing!</p>
-      <img className="homepage-img"
+      <p style="text-align: center">Your one-stop shop for all things amazing!</p>
+      <img style="display: flex, justify-content: center, text-align: center, width: 100%, height: auto"
       src={`${process.env.PUBLIC_URL}/images/retailImages.jpg`} // Use PUBLIC_URL
       alt="Homepage"
     />
